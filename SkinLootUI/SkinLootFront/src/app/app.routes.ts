c157b74190d6df2191e970ad798b935bfe7e1e8a
felipeB2not <felipereis4k@gmail.com>
2025-02-18 11:58:15 -0300

    cards com skins

--- SkinLootUI/SkinLootFront/src/app/app.routes.ts
+++ SkinLootUI/SkinLootFront/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {MenuComponent} from "./pages/menu/menu.component";
+import {SkinCardComponent} from "./pages/skin/skin-card/skin-card.component";
 
 export const routes: Routes = [
   { path: 'menu', component: MenuComponent },
+  { path: 'skins', component: SkinCardComponent },
 
 ];
